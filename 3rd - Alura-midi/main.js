cad6d27675e63276bd55e1fb3bccab1e202eaadf
daianeklein <daiane.klein22@gmail.com>
2022-04-23 19:03:27 -0300

    add files alura midi

--- 3rd - Alura-midi/main.js
+++ 3rd - Alura-midi/main.js
@@ -1,1 +1,13 @@
-document.querySelector('.tecla_pom');
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla')
+
+let contador = 0
+
+while (contador < listaDeTeclas.lenght ) {
+    listaDeTeclas[contador].onclick = tocaSomPom;
+    contador = contador +1
+
+}
