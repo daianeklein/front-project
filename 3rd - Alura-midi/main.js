905383af273d0ee156ccd300b7ba2604de30df44
daianeklein <daiane.klein22@gmail.com>
2022-04-26 20:53:41 -0300

    add main js file

--- 3rd - Alura-midi/main.js
+++ 3rd - Alura-midi/main.js
@@ -1,5 +1,16 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    // if (elemento != null && elemento.localName === ' audio) ...
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado)
+        console.log('Elemento não encontrado')
+    }
+
+    
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -14,6 +25,18 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++ ) {
 
     tecla.onclick = function () {
         tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+        console.log(evento.code);
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+        tecla.classList.add('ativa');
+        }
+    }
+    
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
     }
 
 }
