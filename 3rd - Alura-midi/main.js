9557796db1bc2e6434383cecb4dfb943ff247b6c
daianeklein <daiane.klein22@gmail.com>
2022-04-25 21:10:54 -0300

    add audio

--- 3rd - Alura-midi/main.js
+++ 3rd - Alura-midi/main.js
@@ -1,13 +1,20 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla')
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0
 
-while (contador < listaDeTeclas.lenght ) {
-    listaDeTeclas[contador].onclick = tocaSomPom;
-    contador = contador +1
+for (let contador = 0; contador < listaDeTeclas.length; contador++ ) {
+    
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
 
 }
+
